a646d90cf9fa66d6efb3e47f7cff8ee8830a2715
Mark Richards <mark.richards@guardian.co.uk>
2018-04-04 11:10:55 +0100

    Adding logging to help identity errors

---
+++ iosvalidatereceipts/src/main/scala/com/gu/mobilepurchases/external/Logging.scala
@@ -0,0 +1,18 @@
+package com.gu.mobilepurchases.external
+
+import org.apache.logging.log4j.LogManager
+
+object Logging {
+
+  def logOnThrown[T](function: () => T, messageOnError:String = "", maybeClazz: Option[Class[_]] = None):T  = {
+    try {
+      function()
+    }
+    catch {
+      case t:Throwable => {
+        LogManager.getLogger(maybeClazz.getOrElse(function.getClass)).warn(messageOnError, t)
+        throw t
+      }
+    }
+  }
+}
--- iosvalidatereceipts/src/main/scala/com/gu/mobilepurchases/lambda/ValidateReceiptLambda.scala
+++ iosvalidatereceipts/src/main/scala/com/gu/mobilepurchases/lambda/ValidateReceiptLambda.scala
@@ -6,20 +6,29 @@ import com.amazonaws.services.lambda.runtime.{Context, RequestStreamHandler}
 import com.gu.mobilepurchases.apple.{AppStoreConfig, AppStoreImpl}
 import com.gu.mobilepurchases.config.SsmConfig
 import com.gu.mobilepurchases.validate._
-import com.gu.{AwsIdentity}
-
+import com.gu.AwsIdentity
+import com.gu.mobilepurchases.external.Logging
+import org.apache.logging.log4j.{LogManager, Logger}
+object ValidateReceiptLambda {
+  val logger:Logger = LogManager.getLogger(classOf[ValidateReceiptLambda])
+}
 abstract class ValidateReceiptLambda(
                                       validateReceipts: ValidateReceiptsController,
                                       lambdaApiGateway: LambdaApiGateway
                                     ) extends RequestStreamHandler {
   override def handleRequest(input: InputStream, output: OutputStream, context: Context): Unit = {
-    lambdaApiGateway.execute(input, output, validateReceipts.validate)
+    try {
+      lambdaApiGateway.execute(input, output, validateReceipts.validate)
+    }
+    catch {
+      case t:Throwable => ValidateReceiptLambda.logger.warn(s"Error executing lambda", t)
+    }
   }
 }
 
 object ConfiguredValidateReceiptLambda {
   lazy val ssmConfig = new SsmConfig()
-  lazy val validateReceipts: ValidateReceiptsController = new ValidateReceiptsControllerImpl(
+  lazy val validateReceipts: ValidateReceiptsController = Logging.logOnThrown(() =>  new ValidateReceiptsControllerImpl(
     new ValidateReceiptsValidatorImpl(
       new AppStoreImpl(
         AppStoreConfig(ssmConfig.config, ssmConfig.identity match {
@@ -28,7 +37,7 @@ object ConfiguredValidateReceiptLambda {
         })
       )
     )
-  )
+  ), "Error initialising validate receipts controller")
   lazy val lambdaApiGateway: LambdaApiGateway = new LambdaApiGatewayImpl()
 
 }
