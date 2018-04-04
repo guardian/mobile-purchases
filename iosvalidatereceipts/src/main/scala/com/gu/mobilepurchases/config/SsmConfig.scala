5198f50c61ff9d746d17f95a0525df07ee691ded
Mark Richards <mark.richards@guardian.co.uk>
2018-04-04 11:10:55 +0100

    Adding logging to help identity errors

--- iosvalidatereceipts/src/main/scala/com/gu/mobilepurchases/config/SsmConfig.scala
+++ iosvalidatereceipts/src/main/scala/com/gu/mobilepurchases/config/SsmConfig.scala
@@ -1,11 +1,12 @@
 package com.gu.mobilepurchases.config
 
 import com.gu.conf.{ConfigurationLoader, SSMConfigurationLocation}
+import com.gu.mobilepurchases.external.Logging
 import com.gu.{AppIdentity, AwsIdentity}
 
 class SsmConfig {
-  val identity = AppIdentity.whoAmI(defaultAppName = "mobile-purchases")
-  val config = ConfigurationLoader.load(identity) {
+  val identity: AppIdentity = Logging.logOnThrown(() => AppIdentity.whoAmI(defaultAppName = "mobile-purchases"), "Error feature appidentity")
+  val config = Logging.logOnThrown(() => ConfigurationLoader.load(identity) {
     case identity: AwsIdentity => SSMConfigurationLocation.default(identity)
-  }
+  }, "Error reading config from ssm")
 }
