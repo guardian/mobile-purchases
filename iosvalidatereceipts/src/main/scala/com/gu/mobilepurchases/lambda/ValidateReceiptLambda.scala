c405aa6743042d52606a719b7fde50dd40841fca
Mark Richards <mark.richards@guardian.co.uk>
2018-04-04 13:28:39 +0100

    Wrap logging around LambdaApiGatewayImpl iniitalization

--- iosvalidatereceipts/src/main/scala/com/gu/mobilepurchases/lambda/ValidateReceiptLambda.scala
+++ iosvalidatereceipts/src/main/scala/com/gu/mobilepurchases/lambda/ValidateReceiptLambda.scala
@@ -3,15 +3,17 @@ package com.gu.mobilepurchases.lambda
 import java.io.{InputStream, OutputStream}
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestStreamHandler}
+import com.gu.AwsIdentity
 import com.gu.mobilepurchases.apple.{AppStoreConfig, AppStoreImpl}
 import com.gu.mobilepurchases.config.SsmConfig
-import com.gu.mobilepurchases.validate._
-import com.gu.AwsIdentity
 import com.gu.mobilepurchases.external.Logging
+import com.gu.mobilepurchases.validate._
 import org.apache.logging.log4j.{LogManager, Logger}
+
 object ValidateReceiptLambda {
-  val logger:Logger = LogManager.getLogger(classOf[ValidateReceiptLambda])
+  val logger: Logger = LogManager.getLogger(classOf[ValidateReceiptLambda])
 }
+
 abstract class ValidateReceiptLambda(
                                       validateReceipts: ValidateReceiptsController,
                                       lambdaApiGateway: LambdaApiGateway
@@ -21,14 +23,15 @@ abstract class ValidateReceiptLambda(
       lambdaApiGateway.execute(input, output, validateReceipts.validate)
     }
     catch {
-      case t:Throwable => ValidateReceiptLambda.logger.warn(s"Error executing lambda", t)
+      case t: Throwable => ValidateReceiptLambda.logger.warn(s"Error executing lambda", t)
     }
   }
 }
 
 object ConfiguredValidateReceiptLambda {
   lazy val ssmConfig = new SsmConfig()
-  lazy val validateReceipts: ValidateReceiptsController = Logging.logOnThrown(() =>  new ValidateReceiptsControllerImpl(
+
+  lazy val validateReceipts: ValidateReceiptsController = Logging.logOnThrown(() => new ValidateReceiptsControllerImpl(
     new ValidateReceiptsValidatorImpl(
       new AppStoreImpl(
         AppStoreConfig(ssmConfig.config, ssmConfig.identity match {
@@ -37,8 +40,16 @@ object ConfiguredValidateReceiptLambda {
         })
       )
     )
-  ), "Error initialising validate receipts controller")
-  lazy val lambdaApiGateway: LambdaApiGateway = new LambdaApiGatewayImpl()
+  ),
+    "Error initialising validate receipts controller",
+    Some(classOf[ConfiguredValidateReceiptLambda])
+  )
+
+  lazy val lambdaApiGateway: LambdaApiGateway = Logging.logOnThrown(
+    () => new LambdaApiGatewayImpl(),
+    "Error initialising LambdaApiGatewayImpl",
+    Some(classOf[ConfiguredValidateReceiptLambda])
+  )
 
 }
 
