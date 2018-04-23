5646fcb308268adc3e42d48229a1c5d52a3084f6
Mark Richards <mark.richards@guardian.co.uk>
2018-04-23 16:18:13 +0100

    Fixing metric collation at the end

--- commonlambda/src/main/scala/com/gu/mobilepurchases/shared/cloudwatch/Cloudwatch.scala
+++ commonlambda/src/main/scala/com/gu/mobilepurchases/shared/cloudwatch/Cloudwatch.scala
@@ -1,11 +1,12 @@
 package com.gu.mobilepurchases.shared.cloudwatch
 
-import java.time.{ Duration, Instant }
+import java.time.{Duration, Instant}
 import java.util
 import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.function.Consumer
 
-import com.amazonaws.services.cloudwatch.model.{ MetricDatum, PutMetricDataRequest, StandardUnit }
-import com.amazonaws.services.cloudwatch.{ AmazonCloudWatch }
+import com.amazonaws.services.cloudwatch.model.{MetricDatum, PutMetricDataRequest, StandardUnit}
+import com.amazonaws.services.cloudwatch.AmazonCloudWatch
 
 trait CloudWatch {
   def queueMetric(metricName: String, value: Double): Boolean
@@ -32,11 +33,13 @@ class CloudWatchImpl(stage: String, lambdaname: String, cw: AmazonCloudWatch) ex
 
   def sendMetricsSoFar(): Unit = {
     val arrayOfMetrics: util.ArrayList[MetricDatum] = new util.ArrayList[MetricDatum]()
-    queue.removeAll(arrayOfMetrics)
-    cw.putMetricData(new PutMetricDataRequest()
-      .withNamespace(s"mobile-purchases/$stage/$lambdaname")
-      .withMetricData(arrayOfMetrics)
-    )
+    queue.iterator().forEachRemaining((datum: MetricDatum) => arrayOfMetrics.add(datum))
+    if(arrayOfMetrics.size() > 0) {
+      val request: PutMetricDataRequest = new PutMetricDataRequest()
+        .withNamespace(s"mobile-purchases/$stage/$lambdaname")
+        .withMetricData(arrayOfMetrics)
+      cw.putMetricData(request)
+    }
   }
   def startTimer(metricName: String): Timer = new Timer(metricName, this)
   def meterHttpStatusResponses(metricPrefix: String, code: Int): Unit = queueMetric(s"$metricPrefix-${code % 100}xx", 1)
