9b83926fe02589745c8cf247b8795cb405364591
Mark Richards <37694514+markrichardsg@users.noreply.github.com>
2018-05-14 16:24:33 +0100

    Removing ssm from userpurchases (#20)
    * Removing ssm from userpurchases
    
    * Removing ssm library from user purchases

--- iosuserpurchases/src/main/scala/com/gu/mobilepurchases/userpurchases/lambda/UserPurchasesLambda.scala
+++ iosuserpurchases/src/main/scala/com/gu/mobilepurchases/userpurchases/lambda/UserPurchasesLambda.scala
@@ -4,30 +4,32 @@ import java.time.Clock
 
 import com.amazonaws.services.cloudwatch.{ AmazonCloudWatchAsync, AmazonCloudWatchAsyncClientBuilder }
 import com.gu.mobilepurchases.shared.cloudwatch.{ CloudWatch, CloudWatchImpl, CloudWatchMetrics }
-import com.gu.mobilepurchases.shared.config.{ SsmConfig, SsmConfigLoader }
 import com.gu.mobilepurchases.shared.external.Logging
 import com.gu.mobilepurchases.shared.lambda.AwsLambda
 import com.gu.mobilepurchases.userpurchases.controller.UserPurchasesController
 import com.gu.mobilepurchases.userpurchases.lambda.UserPurchasesLambda.userPurchasesName
 import com.gu.mobilepurchases.userpurchases.persistence.{ ScanamaoUserPurchasesStringsByUserIdColonAppIdImpl, UserPurchaseConfig, UserPurchasePersistenceImpl, UserPurchasePersistenceTransformer }
 import com.gu.mobilepurchases.userpurchases.purchases.UserPurchasesImpl
+import com.gu.{ AppIdentity, AwsIdentity }
 
 object UserPurchasesLambda {
   val userPurchasesName: String = "iosuserpurchases"
 
-  def userPurchasesController(ssmConfig: SsmConfig, clock: Clock, cloudWatch: CloudWatchMetrics): UserPurchasesController = Logging.logOnThrown(() =>
+  def fetchUserPurchaseConfig: UserPurchaseConfig = AppIdentity.whoAmI(defaultAppName = "mobile-purchases") match {
+    case awsIdentity: AwsIdentity => UserPurchaseConfig(awsIdentity.app, awsIdentity.stage, awsIdentity.stack)
+    case _                        => throw new IllegalStateException("Not in aws")
+  }
+
+  def userPurchasesController(userPurchaseConfig: UserPurchaseConfig, clock: Clock, cloudWatch: CloudWatchMetrics): UserPurchasesController = Logging.logOnThrown(() =>
     new UserPurchasesController(new UserPurchasesImpl(new UserPurchasePersistenceImpl(
       ScanamaoUserPurchasesStringsByUserIdColonAppIdImpl(
-        UserPurchaseConfig(
-          ssmConfig.app,
-          ssmConfig.stage,
-          ssmConfig.stack)), new UserPurchasePersistenceTransformer(clock), cloudWatch))), "Error instantiating UserPurchasesLambda", Some(classOf[UserPurchasesLambda]))
-
+        userPurchaseConfig), new UserPurchasePersistenceTransformer(clock), cloudWatch))), "Error instantiating UserPurchasesLambda", Some(classOf[UserPurchasesLambda]))
 }
 
-class UserPurchasesLambda(ssmConfig: SsmConfig, cloudWatch: CloudWatch, clock: Clock) extends AwsLambda(UserPurchasesLambda.userPurchasesController(ssmConfig, clock, cloudWatch), cloudWatch = cloudWatch) {
-  def this(ssmConfig: SsmConfig, amazonCloudWatch: AmazonCloudWatchAsync, clock: Clock) = this(ssmConfig, new CloudWatchImpl(ssmConfig.stage, userPurchasesName, amazonCloudWatch), clock)
-  def this() = this(SsmConfigLoader(userPurchasesName), AmazonCloudWatchAsyncClientBuilder.defaultClient(), Clock.systemUTC())
+class UserPurchasesLambda(userPurchaseConfig: UserPurchaseConfig, cloudWatch: CloudWatch, clock: Clock) extends AwsLambda(UserPurchasesLambda.userPurchasesController(userPurchaseConfig, clock, cloudWatch), cloudWatch = cloudWatch) {
+  def this(userPurchaseConfig: UserPurchaseConfig, amazonCloudWatch: AmazonCloudWatchAsync, clock: Clock) = this(userPurchaseConfig, new CloudWatchImpl(userPurchaseConfig.stage, userPurchasesName, amazonCloudWatch), clock)
+
+  def this() = this(UserPurchasesLambda.fetchUserPurchaseConfig, AmazonCloudWatchAsyncClientBuilder.defaultClient(), Clock.systemUTC())
 
 }
 
--- iosuserpurchases/src/test/scala/com/gu/mobilepurchases/userpurchases/lambda/DelegateUserPurchasesLambdaSpec.scala
+++ iosuserpurchases/src/test/scala/com/gu/mobilepurchases/userpurchases/lambda/DelegateUserPurchasesLambdaSpec.scala
@@ -1,6 +1,6 @@
 package com.gu.mobilepurchases.userpurchases.lambda
 
-import java.io.{ ByteArrayInputStream, ByteArrayOutputStream, InputStream }
+import java.io.{ ByteArrayInputStream, ByteArrayOutputStream, IOException, InputStream }
 
 import com.amazonaws.services.cloudwatch.AmazonCloudWatchAsync
 import com.gu.mobilepurchases.shared.cloudwatch.{ CloudWatchImpl, CloudWatchImplSpec }
@@ -10,12 +10,9 @@ import com.gu.mobilepurchases.shared.lambda.{ ApiGatewayLambdaRequest, ApiGatewa
 import com.gu.mobilepurchases.userpurchases.controller.UserPurchasesController
 import com.gu.mobilepurchases.userpurchases.purchases.{ UserPurchasesRequest, UserPurchasesResponse }
 import com.gu.mobilepurchases.userpurchases.{ UserPurchase, UserPurchaseInterval }
-import com.typesafe.config.{ Config, ConfigFactory }
 import okhttp3.{ Call, Callback, OkHttpClient, Request }
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
-
-import scala.collection.JavaConverters._
 
 class DelegateUserPurchasesLambdaSpec extends Specification with Mockito {
 
@@ -29,30 +26,60 @@ class DelegateUserPurchasesLambdaSpec extends Specification with Mockito {
           "appId" -> "uk.co.guardian.iphone2",
           "userIds" -> "gnmUdid~gia:D39DE40D-CF02-4DFC-96F7-3BF6CA2C1A25,vendorUdid~DB19C9CB-5539-4BF8-B76A-44B9BEB36E22"))))
     "return lambda failure when both fail" in {
-      val config: Config = ConfigFactory.empty()
+
       val userPurchasesController = new UserPurchasesController((userPurchasesRequest: UserPurchasesRequest) => throw new IllegalStateException("lambda failed"))
       val inputStream: InputStream = new ByteArrayInputStream(expectedApiGatewayRequest)
       val outputStream: ByteArrayOutputStream = new ByteArrayOutputStream()
       val client: OkHttpClient = mock[OkHttpClient]
-      new DelegateUserPurchasesLambda(config, userPurchasesController, client, cloudWatchImpl).handleRequest(inputStream, outputStream, null)
+      val mockCall = mock[Call]
+
+      client.newCall(any[Request]()) answers {
+        (_: Any) match {
+          case (request: Request) => {
+            mockCall.enqueue(any[Callback]()) answers {
+              (_: Any) match {
+                case (callback: Callback) =>
+                  callback.onFailure(mockCall, new IOException("Fail delegate"))
+              }
+            }
+            mockCall
+          }
+        }
+      }
+      new DelegateUserPurchasesLambda("http://delegate.invalid/userPurhcases", userPurchasesController, client, cloudWatchImpl).handleRequest(inputStream, outputStream, null)
       val actualResponse: LambdaResponse = LambdaResponse(mapper.readValue[ApiGatewayLambdaResponse](outputStream.toByteArray))
       actualResponse.copy(maybeBody = None) must beEqualTo(LambdaResponse(500, None, Map("Content-Type" -> "text/plain; charset=UTF-8")))
     }
     val expectedLambdaResponse: UserPurchasesResponse = UserPurchasesResponse(Set(UserPurchase("lambdaProductId", "lambdaWebOrderLineItemid", UserPurchaseInterval("lambdaStart", "lambdaEnd"))))
     val expectedDelegateResponse: UserPurchasesResponse = UserPurchasesResponse(Set(UserPurchase("delegateProductId", "delegateWebOrderLineItemid", UserPurchaseInterval("delegateStart", "delegateEnd"))))
     "return lambda success when delegate fails" in {
-      val config: Config = ConfigFactory.empty()
+
       val userPurchasesController = new UserPurchasesController((userPurchasesRequest: UserPurchasesRequest) => expectedLambdaResponse)
       val inputStream: InputStream = new ByteArrayInputStream(expectedApiGatewayRequest)
       val outputStream: ByteArrayOutputStream = new ByteArrayOutputStream()
       val client: OkHttpClient = mock[OkHttpClient]
-      new DelegateUserPurchasesLambda(config, userPurchasesController, client, cloudWatchImpl).handleRequest(inputStream, outputStream, null)
+      val mockCall = mock[Call]
+
+      client.newCall(any[Request]()) answers {
+        (_: Any) match {
+          case (request: Request) => {
+            mockCall.enqueue(any[Callback]()) answers {
+              (_: Any) match {
+                case (callback: Callback) =>
+                  callback.onFailure(mockCall, new IOException("Fail delegate"))
+              }
+            }
+            mockCall
+          }
+        }
+      }
+
+      new DelegateUserPurchasesLambda("http://delegate.invalid/userPurhcases", userPurchasesController, client, cloudWatchImpl).handleRequest(inputStream, outputStream, null)
       val actualResponse: LambdaResponse = LambdaResponse(mapper.readValue[ApiGatewayLambdaResponse](outputStream.toByteArray))
       actualResponse.copy(maybeBody = None) must beEqualTo(LambdaResponse(200, None, Map("Content-Type" -> "application/json; charset=UTF-8")))
       mapper.readValue[UserPurchasesResponse](actualResponse.maybeBody.get) must beEqualTo(expectedLambdaResponse)
     }
     "return delegate when lambda fails" in {
-      val config: Config = ConfigFactory.parseMap(Map("delegate.insecureuserpurchasesurl" -> "http://delegate.invalid/userPurhcases").asJava)
 
       val userPurchasesController = new UserPurchasesController((userPurchasesRequest: UserPurchasesRequest) => throw new IllegalStateException("lambda failed"))
       val inputStream: InputStream = new ByteArrayInputStream(expectedApiGatewayRequest)
@@ -75,14 +102,13 @@ class DelegateUserPurchasesLambdaSpec extends Specification with Mockito {
         }
       }
 
-      new DelegateUserPurchasesLambda(config, userPurchasesController, client, cloudWatchImpl).handleRequest(inputStream, outputStream, null)
+      new DelegateUserPurchasesLambda("http://delegate.invalid/userPurhcases", userPurchasesController, client, cloudWatchImpl).handleRequest(inputStream, outputStream, null)
       val actualResponse: LambdaResponse = LambdaResponse(mapper.readValue[ApiGatewayLambdaResponse](outputStream.toByteArray))
       actualResponse.copy(maybeBody = None) must beEqualTo(LambdaResponse(200, None, Map("delegatekey" -> "delegatevalue")))
       mapper.readValue[UserPurchasesResponse](actualResponse.maybeBody.get) must beEqualTo(expectedDelegateResponse)
     }
 
     "prefer delegate over lambda" in {
-      val config: Config = ConfigFactory.parseMap(Map("delegate.insecureuserpurchasesurl" -> "http://delegate.invalid/userPurhcases").asJava)
 
       val userPurchasesController = new UserPurchasesController((userPurchasesRequest: UserPurchasesRequest) => expectedLambdaResponse)
       val inputStream: InputStream = new ByteArrayInputStream(expectedApiGatewayRequest)
@@ -105,7 +131,7 @@ class DelegateUserPurchasesLambdaSpec extends Specification with Mockito {
         }
       }
 
-      new DelegateUserPurchasesLambda(config, userPurchasesController, client, cloudWatchImpl).handleRequest(inputStream, outputStream, null)
+      new DelegateUserPurchasesLambda("http://delegate.invalid/userPurhcases", userPurchasesController, client, cloudWatchImpl).handleRequest(inputStream, outputStream, null)
       val actualResponse: LambdaResponse = LambdaResponse(mapper.readValue[ApiGatewayLambdaResponse](outputStream.toByteArray))
       actualResponse.copy(maybeBody = None) must beEqualTo(LambdaResponse(200, None, Map("delegatekey" -> "delegatevalue")))
       mapper.readValue[UserPurchasesResponse](actualResponse.maybeBody.get) must beEqualTo(expectedDelegateResponse)
