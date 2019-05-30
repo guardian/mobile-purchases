06a46f6824037f3a412f50153fdb24909a1ca7b2
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2019-05-30 16:16:06 +0100

    Store events in dynamo

--- google/src/pubsub/pubsub.ts
+++ google/src/pubsub/pubsub.ts
@@ -1,5 +1,26 @@
 import {HTTPRequest, HTTPResponse, HTTPResponses} from "../models/apiGatewayHttp";
 import {DeveloperNotification} from "./developerNotification";
+import {SubscriptionEvent} from "../models/subscriptionEvent";
+import DynamoDB from 'aws-sdk/clients/dynamodb';
+import {CredentialProviderChain, SharedIniFileCredentials, ECSCredentials} from "aws-sdk";
+import {DataMapper} from '@aws/dynamodb-data-mapper';
+import {Region} from "../utils/appIdentity";
+
+const ONE_YEAR_IN_SECONDS = 31557600;
+
+const credentialProvider = new CredentialProviderChain([
+    function () { return new ECSCredentials(); },
+    function () { return new SharedIniFileCredentials({
+        profile: "mobile"
+    }); }
+]);
+
+const dynamo = new DynamoDB({
+    region: Region,
+    credentialProvider: credentialProvider
+});
+
+const dynamoMapper = new DataMapper({ client: dynamo });
 
 function parsePayload(body?: string): Error | DeveloperNotification {
     try {
@@ -21,7 +42,26 @@ async function catchingServerErrors(block: () => Promise<HTTPResponse>): Promise
     }
 }
 
-export async function handler(request: HTTPRequest): Promise<HTTPResponse> {
+function toDynamoSubscriptionEvent(notification: DeveloperNotification): SubscriptionEvent {
+    const eventTimestamp = new Date(Number.parseInt(notification.eventTimeMillis)).toISOString();
+    const eventType = notification.subscriptionNotification.notificationType.toString(); // TODO define a Guardian Enum for all the possible events
+    return new SubscriptionEvent(
+        notification.subscriptionNotification.purchaseToken,
+        eventTimestamp + "|" + eventType,
+        eventTimestamp,
+        eventType,
+        "android",
+        notification,
+        null,
+        Math.ceil((Number.parseInt(notification.eventTimeMillis) / 1000) + 7 * ONE_YEAR_IN_SECONDS)
+    );
+}
+
+function storeEvent(event: SubscriptionEvent): Promise<SubscriptionEvent> {
+    return dynamoMapper.put({item: event}).then(result => result.item);
+}
+
+export async function parseAndStore(request: HTTPRequest, storingFunction: (event: SubscriptionEvent) => Promise<SubscriptionEvent>): Promise<HTTPResponse> {
     const secret = process.env.Secret;
     return catchingServerErrors(async () => {
         if (request.queryStringParameters && request.queryStringParameters.secret === secret) {
@@ -30,14 +70,23 @@ export async function handler(request: HTTPRequest): Promise<HTTPResponse> {
                 return HTTPResponses.INVALID_REQUEST
             }
 
-            console.log(notification);
+            const event = toDynamoSubscriptionEvent(notification);
 
-            return HTTPResponses.OK;
+            return storingFunction(event)
+                .then((value) => {
+                    return HTTPResponses.OK
+                }).catch((error) => {
+                    return HTTPResponses.INTERNAL_ERROR
+                });
+
         } else {
             return HTTPResponses.UNAUTHORISED
         }
     });
+}
 
+export async function handler(request: HTTPRequest): Promise<HTTPResponse> {
+    return parseAndStore(request, storeEvent)
 }
 
 
