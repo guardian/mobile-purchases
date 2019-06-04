0208d99413a48560c0d25cdc7efb857dc40e2c19
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2019-06-04 11:24:22 +0100

    Get the transactionId from the receipt info

--- typescript/src/pubsub/apple.ts
+++ typescript/src/pubsub/apple.ts
@@ -2,6 +2,26 @@ import {HTTPRequest, HTTPResponse} from "../models/apiGatewayHttp";
 import {ONE_YEAR_IN_SECONDS, parseStoreAndSend} from "./pubsub";
 import {SubscriptionEvent} from "../models/subscriptionEvent";
 
+export interface ReceiptInfo {
+    transaction_id: string,
+    bid: string,
+    product_id: string,
+    original_transaction_id: string,
+    item_id: string,
+    app_item_id: string,
+    web_order_line_item_id: string,
+    unique_identifier: string,
+    unique_vendor_identifier: string,
+    quantity: string,
+    purchase_date_ms: string,
+    original_purchase_date_ms: string,
+    expires_date: string,
+    is_in_intro_offer_period: string,
+    is_trial_period: string,
+    bvrs: string,
+    version_external_identifier: string
+}
+
 export interface StatusUpdateNotification {
     environment: string,
     notification_type: string,
@@ -10,9 +30,9 @@ export interface StatusUpdateNotification {
     cancellation_date: string,
     web_order_line_item_id: string,
     latest_receipt: string,
-    latest_receipt_info: any,
+    latest_receipt_info: ReceiptInfo,
     latest_expired_receipt: string,
-    latest_expired_receipt_info: any,
+    latest_expired_receipt_info: ReceiptInfo,
     auto_renew_status: boolean,
     auto_renew_adam_id: string,
     auto_renew_product_id: string,
@@ -35,8 +55,11 @@ export function parsePayload(body?: string): Error | StatusUpdateNotification {
 export function toDynamoSubscriptionEvent(notification: StatusUpdateNotification): SubscriptionEvent {
     const now = new Date();
     const eventType = notification.notification_type;
+
+    const receiptInfo = notification.latest_receipt_info || notification.latest_expired_receipt_info;
+
     return new SubscriptionEvent(
-        notification.original_transaction_id,
+        receiptInfo.transaction_id,
         now.toISOString() + "|" + eventType,
         now.toISOString(),
         eventType,
