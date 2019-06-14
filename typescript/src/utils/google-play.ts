1c00ba9e6133eb20ac0d74f0b819bed5a825d6db
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2019-06-14 12:05:16 +0100

    broken s3

--- typescript/src/utils/google-play.ts
+++ typescript/src/utils/google-play.ts
@@ -1,7 +1,28 @@
-import S3 = require("aws-sdk/clients/s3")
+import AWS from 'aws-sdk'
+import S3 from 'aws-sdk/clients/s3'
 import {HttpRequestHeaders, PathParameters} from "../models/apiGatewayHttp";
+import {CredentialProviderChain, ECSCredentials, SharedIniFileCredentials} from "aws-sdk";
+import {Region} from "../utils/appIdentity"
 
-const s3: S3  = new S3();
+
+
+//TODO refactor to common
+const credentialProvider = new CredentialProviderChain([
+    function () { return new ECSCredentials(); },
+    function () { return new SharedIniFileCredentials({
+        profile: "mobile"
+    }); }
+]);
+
+const s3: S3  = new S3({
+    region: Region
+//    credentialProvider: credentialProvider
+});
+
+interface AccessToken {
+    token: string,
+    date: Date
+}
 
 export function getParams(stage: String): S3.Types.GetObjectRequest {
   return {
@@ -10,7 +31,7 @@ export function getParams(stage: String): S3.Types.GetObjectRequest {
   }
 }
 
-export function getAccessToken(params: S3.Types.GetObjectRequest) {
+export function getAccessToken(params: S3.Types.GetObjectRequest) : Promise<AccessToken> {
     console.log(`Attempting to fetch access token from: Bucket: ${params.Bucket} | Key: ${params.Key}`);
     return s3.getObject(params).promise()
         .then( s3OutPut => {
@@ -26,8 +47,8 @@ export function getAccessToken(params: S3.Types.GetObjectRequest) {
         })
 }
 
-export function buildGoogleUrl(subscriptionId: String, purchaseToken: String) {
-    const baseUrl = 'https://www.googleapis.com/androidpublisher/v3/applications/com.guardian/purchases/subscriptions';
-    return `${baseUrl}/${subscriptionId}/tokens/${purchaseToken}';
+export function buildGoogleUrl(subscriptionId: String, purchaseToken: String, packageName: String) {
+    const baseUrl = `https://www.googleapis.com/androidpublisher/v3/applications/${packageName}/purchases/subscriptions`;
+    return `${baseUrl}/${subscriptionId}/tokens/${purchaseToken}`;
 }
 
