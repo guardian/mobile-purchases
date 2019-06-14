9380b89c9b3f4b6b120c3d708841310409a31a74
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2019-06-14 13:56:44 +0100

    extract apple receipt into a model

--- typescript/src/pubsub/apple.ts
+++ typescript/src/pubsub/apple.ts
@@ -2,26 +2,7 @@ import 'source-map-support/register'
 import {HTTPRequest, HTTPResponse} from "../models/apiGatewayHttp";
 import {ONE_YEAR_IN_SECONDS, parseStoreAndSend} from "./pubsub";
 import {SubscriptionEvent} from "../models/subscriptionEvent";
-
-export interface ReceiptInfo {
-    transaction_id: string,
-    bid: string,
-    product_id: string,
-    original_transaction_id: string,
-    item_id: string,
-    app_item_id: string,
-    web_order_line_item_id: string,
-    unique_identifier: string,
-    unique_vendor_identifier: string,
-    quantity: string,
-    purchase_date_ms: string,
-    original_purchase_date_ms: string,
-    expires_date: string,
-    is_in_intro_offer_period: string,
-    is_trial_period: string,
-    bvrs: string,
-    version_external_identifier: string
-}
+import {AppleReceipt} from "../models/appleReceipt";
 
 export interface StatusUpdateNotification {
     environment: string,
@@ -31,9 +12,9 @@ export interface StatusUpdateNotification {
     cancellation_date: string,
     web_order_line_item_id: string,
     latest_receipt: string,
-    latest_receipt_info: ReceiptInfo,
+    latest_receipt_info: AppleReceipt,
     latest_expired_receipt: string,
-    latest_expired_receipt_info: ReceiptInfo,
+    latest_expired_receipt_info: AppleReceipt,
     auto_renew_status: boolean,
     auto_renew_adam_id: string,
     auto_renew_product_id: string,
