9165acab9ad44b24acc5d5c8f93bdd2a5b1ada7f
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2019-06-19 17:19:05 +0100

    try requirng

--- typescript/src/utils/google-play.ts
+++ typescript/src/utils/google-play.ts
@@ -1,45 +1,34 @@
-import S3 from 'aws-sdk/clients/s3'
-import {CredentialProviderChain, ECSCredentials, SharedIniFileCredentials} from "aws-sdk";
-import {Region} from "./appIdentity";
+import aws = require("../utils/aws");
 
+import S3 from 'aws-sdk/clients/s3'
 
 export interface AccessToken {
     token: string,
     date: Date
 }
 
-export function getParams(stage: string): S3.Types.GetObjectRequest {
-    return {
-        Bucket: 'gu-mobile-access-tokens',
-        Key: `${stage}/google-play-developer-api/access_token.json`
-    }
+export function getParams(stage: String): S3.Types.GetObjectRequest {
+  return {
+      Bucket: "gu-mobile-access-tokens",
+      Key: `${stage}/google-play-developer-api/access_token.json`
+  }
 }
 
-export function getAccessToken(params: S3.Types.GetObjectRequest): Promise<AccessToken> {
-    console.log(`Attempting to fetch access token from: Bucket::: ${params.Bucket} | Key: ${params.Key}`);
-    const credentialProvider = new CredentialProviderChain([
-        function () { return new ECSCredentials(); },
-        function () { return new SharedIniFileCredentials({
-            profile: "mobile"
-        }); }
-    ]);
+export function getAccessToken(params: S3.Types.GetObjectRequest) : Promise<AccessToken> {
+    console.log(`Attempting to fetch access token from: Bucket: ${params.Bucket} | Key: ${params.Key}`);
+    return aws.s3.getObject(params).promise()
+        .then( s3OutPut => {
+            if(s3OutPut.Body) {
+                console.log("Got access tk");
 
-
-    const s3: S3  = new S3({
-        region: Region ,
-        credentialProvider: credentialProvider
-    });
-
-    return s3.getObject(params).promise()
-        .then(s3Output => {
-            if (s3Output.Body) {
-                console.log("Got Body")
-                return JSON.parse(s3Output.Body.toString())
+                return JSON.parse(s3OutPut.Body.toString())
             } else {
+                console.log("token errod");
+
                 throw Error("S3 output body was not defined")
             }
         })
-        .catch(error => {
+        .catch( error => {
             console.log(`Failed to get access token from S3 due to: ${error}`);
             throw error
         })
