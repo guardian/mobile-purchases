879f89495a14281e63c5d29140573a4beae3353f
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2019-06-19 11:23:46 +0100

    debug token call

--- typescript/src/utils/google-play.ts
+++ typescript/src/utils/google-play.ts
@@ -6,28 +6,25 @@ interface AccessToken {
     date: Date
 }
 
-export function getParams(stage: String): S3.Types.GetObjectRequest {
-  return {
-      Bucket: "gu-mobile-access-tokens",
-      Key: `${stage}/google-play-developer-api/access_token.json`
-  }
+export function getParams(stage: string): S3.Types.GetObjectRequest {
+    return {
+        Bucket: 'gu-mobile-access-tokens',
+        Key: `${stage}/google-play-developer-api/access_token.json`
+    }
 }
 
-export function getAccessToken(params: S3.Types.GetObjectRequest) : Promise<AccessToken> {
+export function getAccessToken(params: S3.Types.GetObjectRequest): Promise<AccessToken> {
     console.log(`Attempting to fetch access token from: Bucket: ${params.Bucket} | Key: ${params.Key}`);
     return s3Client.getObject(params).promise()
-        .then( s3OutPut => {
-            if(s3OutPut.Body) {
-                console.log("Got access tk");
-
-                return JSON.parse(s3OutPut.Body.toString())
+        .then(s3Output => {
+            if (s3Output.Body) {
+                console.log("Got Body")
+                return JSON.parse(s3Output.Body.toString())
             } else {
-                console.log("token errod");
-
                 throw Error("S3 output body was not defined")
             }
         })
-        .catch( error => {
+        .catch(error => {
             console.log(`Failed to get access token from S3 due to: ${error}`);
             throw error
         })
