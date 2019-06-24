4ace46eb1b1746325eaab7be0b21d715861c83df
Alex Ware <alex.ware@guardian.co.uk>
2019-06-24 15:13:07 +0100

    Run build-tc in node docker container
    This does have yarn but does not have zip

--- build-tc.sh
+++ build-tc.sh
@@ -3,8 +3,6 @@ set -e
 
 export NODE_OPTIONS="--max-old-space-size=2048"
 
-npm install -g yarn
-
 mkdir -p tsc-target
 
 yarn install
@@ -15,10 +13,3 @@ yarn run test
 
 # Will place .js files in tsc-target
 yarn run build
-
-cd tsc-target
-
-for BUNDLE in *.js
-do
-    zip "${BUNDLE%.*}.zip" "$BUNDLE"
-done
