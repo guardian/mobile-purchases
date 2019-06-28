41b5ecc4cad3901837125402c42bf5570ee0c8df
Nathaniel Bennett <nathaniel.bennett@guardian.co.uk>
2019-06-28 15:39:50 +0100

    fix build-tc.sh

--- build-tc.sh
+++ build-tc.sh
@@ -21,4 +21,4 @@ cd tsc-target
 for BUNDLE in *.js
 do
     zip "${BUNDLE%.*}.zip" "$BUNDLE"
-donehttps://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&ved=2ahUKEwiC_dfO_YvjAhVCZcAKHYGADI8QrAIoATAAegQIBRAJ&url=https%3A%2F%2Fstackoverflow.com%2Fquestions%2F26479815%2Fhow-can-i-git-add-a-folder-but-none-of-its-contents&usg=AOvVaw0LEgAvWuLjGND8PaMa9HQn
+done
