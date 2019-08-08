585246d5a8615dbf8e52107a83d2683459402c7c
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2019-08-08 18:22:26 +0100

    Apple's payload doesn't even match their documentation...

--- typescript/src/update-subs/apple.ts
+++ typescript/src/update-subs/apple.ts
@@ -12,14 +12,9 @@ import {msToFormattedString, optionalMsToFormattedString} from "../utils/dates";
 const receiptEndpoint = "https://buy.itunes.apple.com/verifyReceipt"
 const environment = "Production";
 
-interface AppleValidatedRenewalInfo {
-    auto_renew_status: "0" | "1",
-    original_transaction_id: string
-}
-
 interface AppleValidatedReceiptInfo {
     cancellation_date_ms?: string,
-    expires_date_ms: string,
+    expires_date: string,
     original_purchase_date_ms: string,
     original_transaction_id: string
     product_id: string,
@@ -28,11 +23,10 @@ interface AppleValidatedReceiptInfo {
 // there are more fields, I cherry picked what was relevant
 // https://developer.apple.com/documentation/appstorereceipts/responsebody
 interface AppleValidationResponse {
-    environment: string,
+    auto_renew_status: "0" | "1",
     "is-retryable": boolean,
     latest_receipt: string,
-    latest_receipt_info: [AppleValidatedReceiptInfo],
-    pending_renewal_info: [AppleValidatedRenewalInfo],
+    latest_receipt_info: AppleValidatedReceiptInfo,
     status: number
 }
 
@@ -55,38 +49,18 @@ function checkResponseStatus(response: AppleValidationResponse): AppleValidation
         console.error(`Invalid receipt, got status ${response.status} for ${response.latest_receipt}`);
         throw new Error(`Invalid receipt, got status ${response.status}`);
     }
-    if (response.environment !== environment) {
-        console.warn(`Wrong environment. We're in ${environment}, receipt was ${response.environment}`);
-        throw new Error(`Wrong environment`);
-    }
     if (!response.latest_receipt_info) {
         console.error(`No receipt info`);
         throw new Error(`Invalid validation response, no receipt info`);
-    }
-    if (response.latest_receipt_info.length != 1) {
-        console.error(`Invalid latest_receipt_info array length. Got ${response.latest_receipt_info.length} expected 1`);
-        throw new Error(`Invalid latest_receipt_info array length.`);
-    }
-    if (!response.pending_renewal_info) {
-        console.error(`No receipt info`);
-        throw new Error(`Invalid validation response, no receipt info`);
-    }
-    if (response.pending_renewal_info.length != 1) {
-        console.error(`Invalid pending_renewal_info array length. Got ${response.latest_receipt_info.length} expected 1`);
-        throw new Error(`Invalid pending_renewal_info array length.`);
-    }
-    if (response.pending_renewal_info[0].original_transaction_id != response.latest_receipt_info[0].original_transaction_id) {
-        console.error(`original_transaction_id don't match`);
-        throw new Error(`original_transaction_id don't match`);
     }
     return response;
 }
 
 function toAppleSubscription(response: AppleValidationResponse, subRef: AppleSubscriptionReference): AppleSubscription {
-    const latestReceiptInfo = response.latest_receipt_info[0];
+    const latestReceiptInfo = response.latest_receipt_info;
 
     let autoRenewStatus: boolean = false;
-    if (response.pending_renewal_info[0].auto_renew_status === "1") {
+    if (response.auto_renew_status === "1") {
         autoRenewStatus = true;
     }
 
@@ -94,7 +68,7 @@ function toAppleSubscription(response: AppleValidationResponse, subRef: AppleSub
         latestReceiptInfo.original_transaction_id,
         response.latest_receipt,
         msToFormattedString(latestReceiptInfo.original_purchase_date_ms),
-        msToFormattedString(latestReceiptInfo.expires_date_ms),
+        msToFormattedString(latestReceiptInfo.expires_date),
         optionalMsToFormattedString(latestReceiptInfo.cancellation_date_ms),
         autoRenewStatus,
         response,
@@ -107,7 +81,10 @@ function sqsRecordToAppleSubscription(record: SQSRecord): Promise<AppleSubscript
 
     return validateReceipt(subRef)
         .then(response => response.json())
-        .then(body => body as AppleValidationResponse)
+        .then(body => {
+            console.log(body);
+            return body as AppleValidationResponse;
+        })
         .then(checkResponseStatus)
         .then(response => toAppleSubscription(response, subRef))
 }
