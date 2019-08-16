1bd4f088c07816905b245e024733a99b9ac672ed
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2019-08-16 10:57:59 +0100

    Add util date functions to prepare the TTL change

--- typescript/src/utils/dates.ts
+++ typescript/src/utils/dates.ts
@@ -9,3 +9,13 @@ export function optionalMsToFormattedString(ms?: string): string | undefined {
         return undefined;
     }
 }
+
+export function thirtyMonths(from: Date = new Date()): Date {
+    const newDate = new Date(from.getTime());
+    newDate.setMonth(from.getMonth() + 30);
+    return newDate;
+}
+
+export function dateToSecondTimestamp(date: Date): number {
+    return Math.ceil(date.getTime() / 1000);
+}
