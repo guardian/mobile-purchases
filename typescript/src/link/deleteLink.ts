1dd6085ae4b53bc5bc8bb520fc3d17c6c1eab3ba
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2019-09-03 12:09:54 +0100

    Filter TTL events

--- typescript/src/link/deleteLink.ts
+++ typescript/src/link/deleteLink.ts
@@ -1,7 +1,13 @@
 import 'source-map-support/register'
 import {DynamoDBStreamEvent} from "aws-lambda";
 
-export async function handler(parameter: DynamoDBStreamEvent): Promise<any> {
-    console.log(parameter);
-    return parameter;
+export async function handler(event: DynamoDBStreamEvent): Promise<any> {
+    const ttlEvents = event.Records.filter(dynamoEvent => {
+        return dynamoEvent.eventName == "REMOVE" &&
+            dynamoEvent.userIdentity &&
+            dynamoEvent.userIdentity.type == "Service" &&
+            dynamoEvent.userIdentity.principalId == "dynamodb.amazonaws.com"
+    });
+    console.log(JSON.stringify(ttlEvents));
+    return event;
 }
