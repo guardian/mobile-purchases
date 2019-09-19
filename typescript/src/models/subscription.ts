addef9b1994e1eb0c76eb58b922298bab8f7206c
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2019-09-19 09:53:19 +0100

    Refactor types such that the export takes all the fields

--- typescript/src/models/subscription.ts
+++ typescript/src/models/subscription.ts
@@ -1,73 +1,10 @@
-import {hashKey, rangeKey, attribute} from '@aws/dynamodb-data-mapper-annotations';
+import {hashKey, attribute} from '@aws/dynamodb-data-mapper-annotations';
 import {DynamoDbTable} from "@aws/dynamodb-data-mapper";
 import {App, Stage} from "../utils/appIdentity";
 
 
 export class Subscription {
 
-    @hashKey()
-    subscriptionId: string;
-
-    @attribute()
-    startTimestamp?: string;
-
-    @attribute()
-    endTimestamp?: string;
-
-    @attribute()
-    cancellationTimestamp?: string;
-
-    @attribute()
-    autoRenewing?: boolean;
-
-    @attribute()
-    productId?: string;
-
-    @attribute()
-    ttl?: number;
-
-    constructor(subscriptionId: string, startTimestamp?: string, endTimestamp?: string, cancellationTimestamp?: string, autoRenewing?: boolean, productId?: string, ttl?: number) {
-        this.subscriptionId = subscriptionId;
-        this.startTimestamp = startTimestamp;
-        this.endTimestamp = endTimestamp;
-        this.cancellationTimestamp = cancellationTimestamp;
-        this.autoRenewing = autoRenewing;
-        this.productId = productId;
-        this.ttl = ttl;
-    }
-
-    get [DynamoDbTable]() {
-        return `${App}-${Stage}-subscriptions`
-    }
-}
-
-export class GoogleSubscription extends Subscription {
-
-    @attribute()
-    googlePayload?: any;
-
-    constructor(subscriptionId: string, startTimestamp: string, endTimestamp: string, cancellationTimestamp: string | undefined, autoRenewing: boolean, productId: string, ttl: number, googlePayload: any) {
-        super(subscriptionId, startTimestamp, endTimestamp, cancellationTimestamp, autoRenewing, productId, ttl);
-        this.googlePayload = googlePayload;
-    }
-}
-
-export class AppleSubscription extends Subscription {
-
-    @attribute()
-    receipt: string;
-
-    @attribute()
-    applePayload?: any;
-
-    constructor(subscriptionId: string, startTimestamp: string, endTimestamp: string, cancellationTimestamp: string | undefined, autoRenewing: boolean, productId: string, ttl: number, reciept: string, applePayload: any) {
-        super(subscriptionId, startTimestamp, endTimestamp, cancellationTimestamp, autoRenewing, productId, ttl);
-        this.receipt = reciept;
-        this.applePayload = applePayload;
-    }
-}
-
-export class ReadSubscription {
     @hashKey()
     subscriptionId: string;
 
@@ -81,12 +18,61 @@ export class ReadSubscription {
     cancellationTimestamp?: string;
 
     @attribute()
-    autoRenewing?: boolean;
+    autoRenewing: boolean;
+
+    @attribute()
+    productId: string;
+
+    @attribute()
+    googlePayload?: any;
+
+    @attribute()
+    receipt?: string;
+
+    @attribute()
+    applePayload?: any;
+
+    @attribute()
+    ttl?: number;
+
+    constructor(
+        subscriptionId: string,
+        startTimestamp: string,
+        endTimestamp: string,
+        cancellationTimestamp: string | undefined,
+        autoRenewing: boolean,
+        productId: string,
+        googlePayload?: any,
+        receipt?: string,
+        applePayload?: any,
+        ttl?:number
+    ) {
+        this.subscriptionId = subscriptionId;
+        this.startTimestamp = startTimestamp;
+        this.endTimestamp = endTimestamp;
+        this.cancellationTimestamp = cancellationTimestamp;
+        this.autoRenewing = autoRenewing;
+        this.productId = productId;
+        this.googlePayload = googlePayload;
+        this.receipt = receipt;
+        this.applePayload = applePayload;
+        this.ttl = ttl;
+    }
+
+    get [DynamoDbTable]() {
+        return `${App}-${Stage}-subscriptions`
+    }
+}
+
+export class ReadSubscription extends Subscription {
 
     constructor() {
-        this.subscriptionId = "";
-        this.startTimestamp = "";
-        this.endTimestamp = "";
+        super("", "", "", undefined, false, "")
+    }
+
+    setSubscriptionId(subscriptionId: string): ReadSubscription {
+        this.subscriptionId = subscriptionId;
+        return this;
     }
 
     get [DynamoDbTable]() {
