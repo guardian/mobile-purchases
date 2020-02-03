6202ce705274d2f443ed2acff3ccac734bec813c
Francesca Hammond <francesca.hammond@guardian.co.uk>
2020-02-03 14:35:47 +0000

    Add apple revalidate receipt file

---
+++ typescript/src/revalidate-receipts/appleRevalidateReceipts.ts
@@ -0,0 +1,20 @@
+// import {AppleValidationResponse} from "../services/appleValidateReceipts";
+// import {Subscription} from "../models/subscription";
+// import {Option} from "../utils/option";
+//
+// function endTimestampFilter(cancellationDate: Subscription): string | undefined  {
+//     let dateNow: Date = new Date();
+//     let endTimestamp = cancellationDate.endTimestamp
+//
+//     if(endTimestamp == undefined) {
+//         return dateNow.setHours(dateNow.getHours() + 3).toString();
+//     }
+// }
+
+export function handler() {
+ console.log("Running receipt revalidation")
+}
+
+
+
+
