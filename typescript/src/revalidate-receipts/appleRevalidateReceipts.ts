7f1e3512c38e052499d334c6e1c2cca48783f296
Francesca Hammond <francesca.hammond@guardian.co.uk>
2020-02-06 11:40:44 +0000

    Add Scan instead of Query

--- typescript/src/revalidate-receipts/appleRevalidateReceipts.ts
+++ typescript/src/revalidate-receipts/appleRevalidateReceipts.ts
@@ -3,6 +3,7 @@ import {dynamoMapper} from "../utils/aws";
 import {endTimeStampFilterSubscription} from "../models/endTimestampFilter";
 import {equals} from '@aws/dynamodb-expressions';
 import {ReadUserSubscription} from "../models/userSubscription";
+import {TableName} from "aws-sdk/clients/dynamodb";
 
 function endTimestampForQuery(event: ScheduleEvent): Date {
  const defaultDate = plusHours(new Date(), 3);
@@ -18,17 +19,39 @@ interface ScheduleEvent {
 }
 
 export async function handler(event: ScheduleEvent) {
- const query = dynamoMapper.query({
-  valueConstructor: endTimeStampFilterSubscription,
-  indexName: 'ios-endTimestamp-revalidation-index',
-  keyCondition: {subject:'subscriptionId'},
-  filter: {
-   ...equals('2019-10-24T11:38:01.000Z'),
-   subject: 'endTimestamp'
-  }
- });
+  // const query = dynamoMapper.query({valueConstructor: endTimeStampFilterSubscription, indexName: 'ios-endTimestamp-revalidation-index', keyCondition: {}  })
+ // const query = dynamoMapper.query({
+ //  valueConstructor: endTimeStampFilterSubscription,
+ //  indexName: 'ios-endTimestamp-revalidation-index',
+ //  keyCondition: {subject:'subscriptionId'},
+ //  filter: {
+ //   ...equals('2019-10-24T11:38:01.000Z'),
+ //   subject: 'endTimestamp'
+ //  }
+ // });
 
- for await (const subscription of query) {
+ // {
+ //  "TableName": "GameScores",
+ //     "IndexName": "GameTitleIndex",
+ //     "KeyConditionExpression": "GameTitle = :v_title",
+ //     "ExpressionAttributeValues": {
+ //  ":v_title": {"S": "Meteor Blasters"}
+ // },
+ //  "ProjectionExpression": "UserId, TopScore",
+ //     "ScanIndexForward": false
+ // }
+
+ const queryScan = dynamoMapper.scan(
+     endTimeStampFilterSubscription,
+     {
+      startKey: ["subscriptionId"],
+      filter:{
+      ...equals('true'),
+       subject: "autoRenewing"
+      }
+     })
+
+ for await (const subscription of queryScan) {
   console.log(subscription)
  }
 }
