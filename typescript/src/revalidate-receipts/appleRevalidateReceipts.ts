1fe9be572c0c3acdd5342e9b94db76d98038402e
Francesca Hammond <francesca.hammond@guardian.co.uk>
2020-02-06 15:57:08 +0000

    Test subscriptionId
    Correct the indexName
    
    Correct table

--- typescript/src/revalidate-receipts/appleRevalidateReceipts.ts
+++ typescript/src/revalidate-receipts/appleRevalidateReceipts.ts
@@ -4,6 +4,8 @@ import {endTimeStampFilterSubscription} from "../models/endTimestampFilter";
 import {equals} from '@aws/dynamodb-expressions';
 import {ReadUserSubscription} from "../models/userSubscription";
 import {TableName} from "aws-sdk/clients/dynamodb";
+import {ReadSubscriptionEvent} from "../models/subscriptionEvent";
+import {writeSync} from "fs";
 
 function endTimestampForQuery(event: ScheduleEvent): Date {
  const defaultDate = plusHours(new Date(), 3);
@@ -19,19 +21,26 @@ interface ScheduleEvent {
 }
 
 export async function handler(event: ScheduleEvent) {
-console.log("in handler")
- const queryScan = dynamoMapper.scan({
-  valueConstructor: endTimeStampFilterSubscription,
-  indexName: 'ios-endTimestamp-revalidation-index',
-  filter: {
-   ...equals(true),
-   subject: 'autoRenewing'
-  }
- });
+// console.log("in handler")
+//  const queryScan = dynamoMapper.scan({
+//   valueConstructor: endTimeStampFilterSubscription,
+//   indexName: 'ios-endTimestamp-revalidation-index',
+//   filter: {
+//    ...equals(true),
+//    subject: 'autoRenewing'
+//   }
+//  });
+//
+//  for await (const subscription of queryScan) {
+//   console.log(`subscription is: ${subscription}`)
+//  }
+//  console.log(queryScan.count)
+//  return queryScan.count
 
- for await (const subscription of queryScan) {
+ const iterator = dynamoMapper.query(endTimeStampFilterSubscription,{subscriptionId:"100000580817300"}, {indexName: "ios-endTimestamp-revalidation-index"});
+
+ for await (const subscription of iterator) {
   console.log(`subscription is: ${subscription}`)
  }
- console.log(queryScan.count)
- return queryScan.count
 }
+
