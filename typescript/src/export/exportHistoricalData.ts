077b55a2f4ffc25aec73118963a80990e5a94988
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2020-02-17 17:31:40 +0000

    Add optional max messages to fetch

--- typescript/src/export/exportHistoricalData.ts
+++ typescript/src/export/exportHistoricalData.ts
@@ -5,17 +5,21 @@ import {Stage} from "../utils/appIdentity";
 import {plusDays} from "../utils/dates";
 import {GetQueueAttributesRequest, Message, ReceiveMessageRequest} from "aws-sdk/clients/sqs";
 
-async function recursivelyFetchSqsMessages(sqsUrl: string, handleMsg: (message: Message) => void): Promise<void> {
-    const request: ReceiveMessageRequest = {
-        QueueUrl: sqsUrl,
-        MaxNumberOfMessages: 10,
-        WaitTimeSeconds: 3,
-        VisibilityTimeout: 600
-    };
-    const sqsResp = await sqs.receiveMessage(request).promise();
-    if (sqsResp.Messages && sqsResp.Messages.length > 0) {
-        sqsResp.Messages.forEach(handleMsg);
-        return await recursivelyFetchSqsMessages(sqsUrl, handleMsg)
+async function recursivelyFetchSqsMessages(sqsUrl: string, remainingMessagesToFetch: number, handleMsg: (message: Message) => void): Promise<void> {
+    if (remainingMessagesToFetch > 0) {
+        const request: ReceiveMessageRequest = {
+            QueueUrl: sqsUrl,
+            MaxNumberOfMessages: 10,
+            WaitTimeSeconds: 3,
+            VisibilityTimeout: 600
+        };
+        const sqsResp = await sqs.receiveMessage(request).promise();
+        if (sqsResp.Messages && sqsResp.Messages.length > 0) {
+            sqsResp.Messages.forEach(handleMsg);
+            return await recursivelyFetchSqsMessages(sqsUrl, remainingMessagesToFetch - sqsResp.Messages.length, handleMsg)
+        }
+    } else {
+        console.log("Terminating processing early as we've reached the limit of messages to fetch")
     }
 }
 
@@ -57,7 +61,7 @@ async function getNumberOfMessagesNotVisible(sqsUrl: string): Promise<number> {
     }
 }
 
-export async function handler(params: {date: string}): Promise<any> {
+export async function handler(params: {date: string, maxMessagesToFetch?: number}): Promise<any> {
     const bucket = process.env['ExportBucket'];
     if (!bucket) throw new Error('Variable ExportBucket must be set');
 
@@ -70,6 +74,8 @@ export async function handler(params: {date: string}): Promise<any> {
     }
 
     let zippedStream = zlib.createGzip();
+
+    const maxMessagesToFetch = params.maxMessagesToFetch ?? Number.MAX_VALUE;
 
     const yesterday = params.date ?? plusDays(new Date(), -1).toISOString().substr(0,10);
     const prefix = (Stage === "PROD") ? "data" : "code-data";
@@ -98,7 +104,7 @@ export async function handler(params: {date: string}): Promise<any> {
         }
     }
 
-    await recursivelyFetchSqsMessages(sqsUrl, handleOneMessage);
+    await recursivelyFetchSqsMessages(sqsUrl, maxMessagesToFetch, handleOneMessage);
 
     zippedStream.end();
     await managedUpload.promise();
