04e3b564abc48620f54283874b303f0780f1b2e3
Marjan K <marjank@30728.gnm.int>
2021-01-25 10:03:14 +0000

    remove un necessary field

--- typescript/src/models/GracefulProcessingError.ts
+++ typescript/src/models/GracefulProcessingError.ts
@@ -1,10 +1,8 @@
 export class GracefulProcessingError extends Error {
     message: string;
-    shouldRetry?: boolean;
 
     constructor(message: string, shouldRetry?: boolean) {
         super(message);
         this.message = message;
-        this.shouldRetry = shouldRetry;
     }
 }
