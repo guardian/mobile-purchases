1c7d2b1a213d329858dcce4fd1418c9e3dd76b79
Paul Roberts <paul.roberts@theguardian.com>
2021-10-26 17:58:42 +0100

    support the test-lambda via webpack

--- typescript/src/test-launcher/test-launcher.ts
+++ typescript/src/test-launcher/test-launcher.ts
@@ -1,8 +1,10 @@
 import * as fs from "fs";
 
+import path from 'path';
+
 if(process.argv.length == 4) {
     let rawPayload = fs.readFileSync(`mobile-purchases-payload/${process.argv[3]}`)
-    import(process.argv[2]).then( module => {
+    import(path.resolve(__dirname, process.argv[2])).then( module => {
         module.handler(JSON.parse(rawPayload.toString()))
             .then((res: any) => {
                 console.log(`Completed: result: ${JSON.stringify(res)}`)
