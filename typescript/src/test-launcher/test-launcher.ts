62ba53f5a3521e85779c98209fe8c7499494ec8e
Paul Roberts <paul.roberts@theguardian.com>
2021-10-27 13:21:03 +0100

    fixup the test launcher import path

--- typescript/src/test-launcher/test-launcher.ts
+++ typescript/src/test-launcher/test-launcher.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 if(process.argv.length == 4) {
     let rawPayload = fs.readFileSync(`mobile-purchases-payload/${process.argv[3]}`)
-    import(path.resolve(__dirname, process.argv[2])).then( module => {
+    import(`../${process.argv[2]}`).then( module => {
         module.handler(JSON.parse(rawPayload.toString()))
             .then((res: any) => {
                 console.log(`Completed: result: ${JSON.stringify(res)}`)
