eea377b0f1fd85500471fcedfa8e3959613072bb
andrewHEguardian <Andrew.howe-ely@guardian.co.uk>
2023-03-21 17:16:42 +0000

    Use mocks in cancelSubscription test

--- typescript/tests/soft-opt-ins/cancelSubscription.test.ts
+++ typescript/tests/soft-opt-ins/cancelSubscription.test.ts
@@ -1,5 +1,6 @@
-import { DynamoDBRecord } from "aws-lambda";
-import { getCancellationRecords } from "../../src/soft-opt-ins/cancelSubscription";
+import { DynamoDBRecord, DynamoDBStreamEvent } from "aws-lambda";
+import { ReadUserSubscription } from "../../src/models/userSubscription";
+import { getCancellationRecords, handler } from "../../src/soft-opt-ins/cancelSubscription";
 
 
 describe("getCancellationRecords", () => {
@@ -16,15 +17,97 @@ describe("getCancellationRecords", () => {
 	})
 });
 
-const expectedSoftOptInMessage = {
-	identityId: "42",
-	eventType: "Cancellation",
-	productName: "InAppPurchase"
-};
+jest.mock('@aws/dynamodb-data-mapper', () => {
+	const actualDataMapper = jest.requireActual('@aws/dynamodb-data-mapper');
+
+	const queryFn = jest.fn();
+
+	return {
+		...actualDataMapper,
+		DataMapper: jest.fn().mockImplementation(() => ({
+			batchPut: jest.fn(),
+			get: jest.fn(),
+			put: jest.fn().mockResolvedValue(undefined),
+			delete: jest.fn(),
+			query: queryFn,
+			scan: jest.fn(),
+			update: jest.fn(),
+		})),
+		setMockQuery: (mockImplementation: (arg0: any) => any) => {
+			queryFn.mockImplementation(async function* (params) {
+				const iterator = mockImplementation(params);
+				for await (const item of iterator) {
+					yield item;
+				}
+			});
+		},
+	};
+});
+jest.mock('aws-sdk/clients/sqs', () => {
+	const mockSQS = {
+		sendMessage: jest.fn().mockReturnValue({ promise: jest.fn() }),
+	};
+
+	return jest.fn(() => mockSQS)
+})
+
+const setMockQuery = require('@aws/dynamodb-data-mapper').setMockQuery;
+
+describe("handler", () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+
+	it('should process cancel records, put message on queue', async () => {
+
+		// get the mock instances
+		const mockDataMapper = new (require('@aws/dynamodb-data-mapper').DataMapper)();
+		const mockSQS = new (require('aws-sdk/clients/sqs'))();
+
+		setMockQuery(async function* (params: { keyCondition: any; indexName: any; }) {
+			yield {
+				subscriptionId: '1',
+				userId: '123',
+				startTimestamp: "2023-03-14 07:24:38 UTC",
+				endTimestamp: "2025-03-01 07:24:38 UTC",
+				cancellationTimestamp: null,
+				autoRenewing: false,
+				productId: '',
+				platform: undefined,
+				freeTrial: undefined,
+				billingPeriod: undefined,
+				googlePayload: undefined,
+				receipt: undefined,
+				applePayload: undefined,
+				ttl: undefined,
+			};
+		});
+
+		const event: DynamoDBStreamEvent = {
+			Records: [cancellationRecord, insertDynamoRecord, updateDynamoRecord]
+		}
+
+		await handler(event);
+		expect(mockDataMapper.query).toHaveBeenCalledTimes(1);
+		expect(mockDataMapper.query).toHaveBeenCalledWith(ReadUserSubscription, { subscriptionId: "1" }, { indexName: "subscriptionId-userId" });
+
+		expect(mockSQS.sendMessage).toHaveBeenCalledTimes(1);
+		const expectedSoftOptInMessage = {
+			identityId: "123",
+			eventType: "Cancellation",
+			productName: "InAppPurchase"
+		};
+		const expectedQueueMessageParams = {
+			QueueUrl: 'soft-opt-in-consent-setter-queue-CODE',
+			MessageBody: JSON.stringify(expectedSoftOptInMessage),
+		};
+
+		expect(mockSQS.sendMessage).toHaveBeenCalledWith(expectedQueueMessageParams);
+	})
+})
+
 
 const updateDynamoRecord: DynamoDBRecord = {
-	eventID: "1",
-	eventVersion: "1.0",
 	dynamodb: {
 		Keys: {
 			subscriptionId: {
@@ -36,20 +119,12 @@ const updateDynamoRecord: DynamoDBRecord = {
 				S: "1",
 			}
 		},
-		StreamViewType: "NEW_IMAGE",
-		SequenceNumber: "111",
-		SizeBytes: 26,
 	},
-	awsRegion: "",
 	eventName: "MODIFY",
-	eventSourceARN: "",
-	eventSource: "aws:dynamodb",
 }
 
 
 const insertDynamoRecord: DynamoDBRecord = {
-	eventID: "1",
-	eventVersion: "1.0",
 	dynamodb: {
 		Keys: {
 			subscriptionId: {
@@ -61,19 +136,11 @@ const insertDynamoRecord: DynamoDBRecord = {
 				S: "1",
 			},
 		},
-		StreamViewType: "NEW_IMAGE",
-		SequenceNumber: "111",
-		SizeBytes: 26,
 	},
-	awsRegion: "",
 	eventName: "INSERT",
-	eventSourceARN: "",
-	eventSource: "aws:dynamodb",
 }
 
 const cancellationRecord: DynamoDBRecord = {
-	eventID: "1",
-	eventVersion: "1.0",
 	dynamodb: {
 		Keys: {
 			subscriptionId: {
@@ -88,12 +155,6 @@ const cancellationRecord: DynamoDBRecord = {
 				N: "1",
 			},
 		},
-		StreamViewType: "NEW_IMAGE",
-		SequenceNumber: "111",
-		SizeBytes: 26,
 	},
-	awsRegion: "",
 	eventName: "MODIFY",
-	eventSourceARN: "",
-	eventSource: "aws:dynamodb",
 };
