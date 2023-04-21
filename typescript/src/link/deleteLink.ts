36c898f743fd9a85612fc6732d8ffffe485c149d
Jonathan Flynn <jon.flynn@guardian.co.uk>
2023-04-21 17:47:16 +0100

    turn on flag for testing

--- typescript/src/link/deleteLink.ts
+++ typescript/src/link/deleteLink.ts
@@ -81,7 +81,7 @@ export async function handler(event: DynamoDBStreamEvent): Promise<any> {
     let records = 0;
     let rows = 0;
 
-    const featureFlag = false;
+    const featureFlag = true;
 
     for (const subscriptionId of subscriptionIds) {
         const userLinksIterator = await getUserLinks(subscriptionId);
