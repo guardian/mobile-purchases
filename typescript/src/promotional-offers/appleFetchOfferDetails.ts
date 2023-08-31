ee15f590f8939b5fcf3da75d09ab269e8d89d5b8
Pascal Honoré <pascal@alseyn.net>
2023-08-31 11:35:33 +0100

    lambda answer

--- typescript/src/promotional-offers/appleFetchOfferDetails.ts
+++ typescript/src/promotional-offers/appleFetchOfferDetails.ts
@@ -3,5 +3,14 @@ import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
 import { HTTPResponses } from '../models/apiGatewayHttp';
 
 export async function handler(request: APIGatewayProxyEvent): Promise<APIGatewayProxyResult>  {
-    return Promise.resolve(HTTPResponses.INTERNAL_ERROR);
+    const answer = {
+        statusCode: 200,
+        body: JSON.stringify({
+            nonce: "c5b21eae-81e4-4b4a-be81-13cb5853984d",
+            timestamp: 1693478012,
+            keyIdentifier: "339f60b9af61",
+            signature: "284A0C2BCFEF44809BAC4C286708DA51"
+        })
+    }
+    return Promise.resolve(answer);
 }
