00522c380e574eb45f555986584a0eb8b9dff1e1
Pascal Honoré <pascal@alseyn.net>
2023-09-01 11:36:19 +0100

    payloadToResponse update

--- typescript/src/promotional-offers/appleFetchOfferDetails.ts
+++ typescript/src/promotional-offers/appleFetchOfferDetails.ts
@@ -16,10 +16,28 @@ interface Response {
 }
 
 function payloadToResponse(payload: HttpRequestPayload): Response {
+
+    // https://developer.apple.com/documentation/storekit/in-app_purchase/original_api_for_in-app_purchase/subscriptions_and_offers/generating_a_signature_for_promotional_offers
+    // appBundleID         :
+    // keyIdentifier       :
+    // productIdentifier   : (from request payload)
+    // offerIdentifier     : (from request payload)
+    // applicationUsername : (from request payload as username)
+    // nonce               : (server generated)
+    // timestamp           : (server generated)
+
+    const appBundleID = 'appBundleID';
+    const keyIdentifier = 'keyIdentifier';
+    const productIdentifier = payload.productIdentifier;
+    const offerIdentifier = payload.offerIdentifier;
+    const applicationUsername = payload.username;
+    const nonce = "6e090f4d-484d-490d-94df-18cad3a73138";
+    const timestamp = Date. now(); // generates the currenty unixtime in milliseconds
+
     return {
-        nonce: "c5b21eae-81e4-4b4a-be81-13cb5853984d",
-        timestamp: 1693478012,
-        keyIdentifier: "339f60b9af61",
+        nonce: nonce,
+        timestamp: timestamp,
+        keyIdentifier: keyIdentifier,
         signature: "284A0C2BCFEF44809BAC4C286708DA51"
     };
 } 
