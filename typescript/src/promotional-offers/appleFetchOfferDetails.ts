60543f1911f29e0304a5447d293c12090aaff5b7
Pascal Honoré <pascal@alseyn.net>
2023-09-01 10:43:10 +0100

    introduce interfaces

--- typescript/src/promotional-offers/appleFetchOfferDetails.ts
+++ typescript/src/promotional-offers/appleFetchOfferDetails.ts
@@ -8,19 +8,38 @@ interface HttpRequestPayload {
     offerIdentifier: string
 }
 
+interface Response {
+    nonce: string,
+    timestamp: number,
+    keyIdentifier: string,
+    signature: string
+}
+
+function payloadToResponse(payload: HttpRequestPayload): Response {
+    return {
+        nonce: "c5b21eae-81e4-4b4a-be81-13cb5853984d",
+        timestamp: 1693478012,
+        keyIdentifier: "339f60b9af61",
+        signature: "284A0C2BCFEF44809BAC4C286708DA51"
+    };
+} 
+
 export async function handler(request: APIGatewayProxyEvent): Promise<APIGatewayProxyResult>  {
     const requestBody = request.body;
-    console.log("[a19489e1] " + requestBody);
-    const payloadObject = JSON.parse(requestBody ?? "")
-    console.log("[a19489e2] " + JSON.stringify(payloadObject));
-    const answer = {
-        statusCode: 200,
-        body: JSON.stringify({
-            nonce: "c5b21eae-81e4-4b4a-be81-13cb5853984d",
-            timestamp: 1693478012,
-            keyIdentifier: "339f60b9af61",
-            signature: "284A0C2BCFEF44809BAC4C286708DA51"
-        })
+    const payloadObject = JSON.parse(requestBody ?? "");
+    try {
+        const responseObject = payloadToResponse(payloadObject);
+        const answer = {
+            statusCode: 200,
+            body: JSON.stringify(responseObject)
+        }
+        return Promise.resolve(answer);
+    } catch (error) {
+        const answer = {
+            statusCode: 500,
+            body: "error while computing response"
+        }
+        return Promise.resolve(answer);
     }
-    return Promise.resolve(answer);
+
 }
