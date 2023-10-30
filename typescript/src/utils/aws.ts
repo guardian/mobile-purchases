ef86415dbf85e154a218f881ccba13ec6ddee2e5
Pascal Honoré <pascal@alseyn.net>
2023-10-30 17:07:33 +0000

    arrow function

--- typescript/src/utils/aws.ts
+++ typescript/src/utils/aws.ts
@@ -130,7 +130,7 @@ export async function putMetric(metricName: string, value: number = 1.0): Promis
 
 export function sendToSqs(queueUrl: string, event: any, delaySeconds?: number): Promise<PromiseResult<Sqs.SendMessageResult, AWSError>> {
     console.log(`[fddc199d(1)] sendToSqs ${JSON.stringify(event)}`);
-    const messageBody = JSON.stringify(event, function(k, v){return v === undefined ? null : v ;});
+    const messageBody = JSON.stringify(event, (k, v) => v === undefined ? null : v);
     console.log(`[fddc199d(2)] sendToSqs ${messageBody}`);
     return sqs.sendMessage({
         QueueUrl: queueUrl,
