27de9ec71b795c0a80955b130552a6787554102c
Tom Wadeson <3607811+tomwadeson@users.noreply.github.com>
2024-01-26 12:06:29 +0000

    Read subscription status from state before hitting Google

--- typescript/src/subscription-status/googleSubStatus.ts
+++ typescript/src/subscription-status/googleSubStatus.ts
@@ -7,9 +7,11 @@ import {
 import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
 import {fetchGoogleSubscription} from "../services/google-play";
 import {optionalMsToDate} from "../utils/dates";
-import {Option} from "../utils/option";
+import {ReadSubscription} from "../models/subscription";
+import {dynamoMapper} from "../utils/aws";
+import {createHash} from 'crypto'
 
-interface SubscriptionStatusResponse {
+type SubscriptionStatus = {
     "subscriptionHasLapsed": boolean
     "subscriptionExpiryDate": Date
 }
@@ -35,38 +37,66 @@ export async function handler(request: APIGatewayProxyEvent): Promise<APIGateway
 
     const purchaseToken = getPurchaseToken(request.headers);
     const subscriptionId = getSubscriptionId(request.pathParameters);
+    const packageName = googlePackageName(request.headers);
+
     if (purchaseToken && subscriptionId) {
-        const packageName = googlePackageName(request.headers);
-        console.log(`Searching for valid ${subscriptionId} subscription for Android app with package name: ${packageName}`);
-
+        const purchaseTokenHash = createHash('sha256').update(purchaseToken).digest('hex')
+        console.log(`Searching for valid ${subscriptionId} subscription for Android app with package name: ${packageName}, for purchaseToken hash: ${purchaseTokenHash}`)
         try {
-            const subscription = await fetchGoogleSubscription(subscriptionId, purchaseToken, packageName);
-
-            const subscriptionExpiryDate: Option<Date> = optionalMsToDate(subscription?.expiryTimeMillis);
-            if (subscriptionExpiryDate !== null) {
-                const now: Date = new Date(Date.now());
-                const subscriptionHasLapsed: boolean = now > subscriptionExpiryDate;
-                const responseBody: SubscriptionStatusResponse = {
-                    "subscriptionHasLapsed": subscriptionHasLapsed,
-                    "subscriptionExpiryDate": subscriptionExpiryDate
-                };
-                console.log(`Successfully retrieved subscription details from Play Developer API. Response body will be: ${JSON.stringify(responseBody)}`);
-                return {statusCode: 200, body: JSON.stringify(responseBody)};
+            const subscriptionStatus = 
+                await getSubscriptionStatusFromDynamo(purchaseToken, purchaseTokenHash) ?? 
+                await getSubscriptionStatusFromGoogle(subscriptionId, purchaseToken, packageName, purchaseTokenHash)
+            
+            if (subscriptionStatus !== null) {
+                return { statusCode: 200, body: JSON.stringify(subscriptionStatus) }
             } else {
-                console.log(`Failed to establish expiry time of subscription`);
+                console.log(`No subscription found found for purchaseToken hash: ${purchaseTokenHash}`)
                 return HTTPResponses.NOT_FOUND;
             }
         } catch (error: any) {
-            if (error.statusCode === 410) {
-                console.log(`Purchase expired a very long time ago`);
-                return HTTPResponses.NOT_FOUND;
-            } else {
-                console.log(`Serving an Internal Server Error due to: ${error.toString().split('/tokens/')[0]}`);
-                return HTTPResponses.INTERNAL_ERROR;
-            }
+            console.log(`Serving an Internal Server Error due to: ${error.toString().split('/tokens/')[0]}`)
+            return HTTPResponses.INTERNAL_ERROR
         }
     } else {
-        return HTTPResponses.INVALID_REQUEST;
+        return HTTPResponses.INVALID_REQUEST
     }
+}
 
+
+async function getSubscriptionStatusFromGoogle(subscriptionId: string, purchaseToken: string, packageName: string, purchaseTokenHash: string): Promise<SubscriptionStatus | null> {
+    console.log(`Fetching subscription from Google for purchaseToken hash: ${purchaseTokenHash}`)
+    const subscription = await fetchGoogleSubscription(subscriptionId, purchaseToken, packageName)
+    const subscriptionExpiryDate = optionalMsToDate(subscription?.expiryTimeMillis)
+    const googleSubscriptionStatus = subscriptionExpiryDate ? subscriptionStatus(subscriptionExpiryDate) : null
+    console.log(`Google SubscriptionStatus for purchaseToken hash: ${purchaseTokenHash}: ${JSON.stringify(googleSubscriptionStatus)}`)
+    return googleSubscriptionStatus
+}
+
+async function getSubscriptionStatusFromDynamo(purchaseToken: string, purchaseTokenHash: string): Promise<SubscriptionStatus | null> {
+    try {
+        console.log(`Fetching subscription from Dynamo for purchaseToken hash: ${purchaseTokenHash}`)
+        let itemToQuery = new ReadSubscription()
+        itemToQuery.setSubscriptionId(purchaseToken)
+        const subscription = await dynamoMapper.get(itemToQuery)
+        const subscriptionExpiryDate = new Date(subscription.endTimestamp)
+        const dynamoSubscriptionStatus = subscriptionStatus(subscriptionExpiryDate)
+        console.log(`Dynamo SubscriptionStatus for purchaseToken hash: ${purchaseTokenHash}: ${JSON.stringify(dynamoSubscriptionStatus)}`)
+        return dynamoSubscriptionStatus
+    } catch (error: any) {
+        if (error.name === 'ItemNotFoundException') {
+            console.log(`No subscription found in Dynamo with purchaseToken hash: ${purchaseTokenHash}`)
+        } else {
+            console.log(`The following Dynamo error occurred when attempting to retrieve a subscription with purchaseToken hash: ${purchaseTokenHash}: ${error}`)
+        }
+        // All exceptions are swallowed here as we fall-back on the Google API for all failure modes (including cache misses)
+        return null
+    }
+}
+
+function subscriptionStatus(expiryDate: Date): SubscriptionStatus {
+    const now = new Date(Date.now())
+    return {
+        "subscriptionHasLapsed": now > expiryDate,
+        "subscriptionExpiryDate": expiryDate
+    }
 }
