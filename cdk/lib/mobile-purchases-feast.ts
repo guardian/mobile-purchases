33f7204bf8b71f25ea74c234eff17fd8d54c0c36
Michael Jacobson <michael.jacobson@guardian.co.uk>
2024-03-04 14:08:50 +0000

    add `mobile-purchases-feast` to ci pipeline

--- cdk/lib/mobile-purchases-feast.ts
+++ cdk/lib/mobile-purchases-feast.ts
@@ -2,7 +2,7 @@ import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
 import { GuStack } from "@guardian/cdk/lib/constructs/core";
 import type { App } from "aws-cdk-lib";
 
-export class MobilePurchases extends GuStack {
+export class MobilePurchasesFeast extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
   }
