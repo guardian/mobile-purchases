e1a3e939c6fba4c92da0a1521c1c2a75bfe5fbf2
Michael Jacobson <michael.jacobson@guardian.co.uk>
2024-06-03 13:41:25 +0100

    define skeleton handler function

---
+++ typescript/src/feast/update-subs/google.ts
@@ -0,0 +1,9 @@
+import { SQSEvent } from "aws-lambda";
+
+export function buildHandler(): (event: SQSEvent) => void {
+    return (event: SQSEvent) => {
+        console.log(`Received SQS event: ${event}`)
+    }
+}
+
+export const handler = buildHandler();
