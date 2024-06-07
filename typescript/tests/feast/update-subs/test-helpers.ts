c03885bd359610421c2e1b201c1ebec837829276
Tom Wey <tom.wey@theguardian.com>
2024-06-07 11:57:31 +0100

    Refactor this to make testing (and incorporating changes for Feast) easier
    And add a test to check the wiring.

---
+++ typescript/tests/feast/update-subs/test-helpers.ts
@@ -0,0 +1,26 @@
+import type { SQSEvent } from "aws-lambda";
+import type { SubscriptionReference } from "../../../src/models/subscriptionReference";
+
+export const buildSqsEvent = (subscriptions: SubscriptionReference[]): SQSEvent => {
+    const records = subscriptions.map(subscription =>
+    ({
+        messageId: "",
+        receiptHandle: "",
+        body: JSON.stringify(subscription),
+        attributes: {
+            ApproximateReceiveCount: "",
+            SentTimestamp: "",
+            SenderId: "",
+            ApproximateFirstReceiveTimestamp: "",
+        },
+        messageAttributes: {},
+        md5OfBody: "",
+        eventSource: "",
+        eventSourceARN: "",
+        awsRegion: "",
+    }))
+
+    return {
+        Records: records
+    }
+}
