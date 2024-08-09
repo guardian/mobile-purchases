7e6fe7251cc000e5b133d4bfbd9cd4f9353eb8d9
Michael McNamara <michael.mcnamara@theguardian.com>
2024-08-09 10:30:23 +0100

    try removing pre-existing cdk

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,10 +1,10 @@
 import "source-map-support/register";
 import { GuRoot } from "@guardian/cdk/lib/constructs/root";
 import {FeastGoogleAcquisitionsEvent} from "../lib/feast-google-acquisitions-event";
-import { MobilePurchasesFeast } from "../lib/mobile-purchases-feast";
+//import { MobilePurchasesFeast } from "../lib/mobile-purchases-feast";
 
 const app = new GuRoot();
-new MobilePurchasesFeast(app, "MobilePurchasesFeast-CODE", { stack: "mobile", stage: "CODE", env: { region: "eu-west-1" } });
-new MobilePurchasesFeast(app, "MobilePurchasesFeast-PROD", { stack: "mobile", stage: "PROD", env: { region: "eu-west-1" } });
+//new MobilePurchasesFeast(app, "MobilePurchasesFeast-CODE", { stack: "mobile", stage: "CODE", env: { region: "eu-west-1" } });
+//new MobilePurchasesFeast(app, "MobilePurchasesFeast-PROD", { stack: "mobile", stage: "PROD", env: { region: "eu-west-1" } });
 new FeastGoogleAcquisitionsEvent(app, "FeastGoogleAcquisitionsEvent-CODE" , { stack: "mobile", stage: "CODE", env: { region: "eu-west-1" } });
 new FeastGoogleAcquisitionsEvent(app, "FeastGoogleAcquisitionsEvent-PROD" , { stack: "mobile", stage: "PROD", env: { region: "eu-west-1" } });
