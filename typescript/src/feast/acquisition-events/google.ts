93d5ace55316840e5748b796f213bc660e7082e1
Michael McNamara <michael.mcnamara@theguardian.com>
2024-09-23 15:50:23 +0100

    remove commented code

--- typescript/src/feast/acquisition-events/google.ts
+++ typescript/src/feast/acquisition-events/google.ts
@@ -5,11 +5,4 @@ export const handler = async (event: SQSEvent): Promise<String> => {
     const message: String = 'Feast Google Acquisition Events Lambda has been called';
     console.log(message)
     return message;
-    // const eventualEnsuredIdentityAccount = event.Records.flatMap(
-        // async (sqsRecord: SQSRecord) => {
-        //     console.log(
-        //         `Hello World`,
-        //     );
-        // });
-    // await Promise.all<void>(eventualEnsuredIdentityAccount);
 }
