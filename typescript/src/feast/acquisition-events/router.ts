fd8edd8ac80123ea37537b5ff072ee220f4dfc46
Michael McNamara <michael.mcnamara@theguardian.com>
2024-09-26 15:48:22 +0100

    add config for routing lambda to read dynamo DB and write messages to SQS queues

--- typescript/src/feast/acquisition-events/router.ts
+++ typescript/src/feast/acquisition-events/router.ts
@@ -1,8 +1,7 @@
-import type { SQSEvent } from 'aws-lambda';
-import {buildHandler} from "../pubsub/google";
+import type { DynamoDBStreamEvent } from 'aws-lambda';
 
-export const handler = async (event: SQSEvent): Promise<String> => {
-    const message: String = 'Feast Apple Acquisition Events Lambda has been called';
+export const handler = async (event: DynamoDBStreamEvent): Promise<String> => {
+    const message: String = 'Feast Acquisition Events Router Lambda has been called';
     console.log(message)
     return message;
 }
