b93c8223a02e2f0c0e1a4f2d2c8b9dc8eae0d155
Pascal Honoré <pascal@alseyn.net>
2024-11-11 15:23:45 +0000

    Introspection of DynamoDBStreamEvent for Feast IAP acquisitions

--- typescript/src/feast/acquisition-events/router.ts
+++ typescript/src/feast/acquisition-events/router.ts
@@ -1,7 +1,23 @@
-import type { DynamoDBStreamEvent } from 'aws-lambda';
+import type { DynamoDBRecord, DynamoDBStreamEvent } from 'aws-lambda';
 
 export const handler = async (event: DynamoDBStreamEvent): Promise<String> => {
-    const message: String = 'Feast Acquisition Events Router Lambda has been called';
-    console.log(message)
-    return message;
+    console.log('[c9900d41] Feast Acquisition Events Router Lambda has been called');
+
+    const records = event.Records; // retrieve records from DynamoDBStreamEvent
+
+    let processedCount = 0;
+
+    const processRecordPromises = records.map(async (record: DynamoDBRecord) => {
+        const eventName = record.eventName;
+        const identityId = record?.dynamodb?.NewImage?.userId?.S || "";
+        const subscriptionId = record?.dynamodb?.NewImage?.subscriptionId?.S || "";
+        console.log(`Processing: ${eventName} record for identityId: ${identityId} and subscriptionId: ${subscriptionId}`);
+        processedCount ++;
+    });
+
+    await Promise.all(processRecordPromises);
+
+    console.log(`Processed ${processedCount} records from DynamoDBStreamEvent`);
+
+    return 'Success';
 }
