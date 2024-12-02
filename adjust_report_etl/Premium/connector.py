8040b4b43cdb4e1dc7477ce618f6747efdea8e6c
David Pepper <36296660+david-pepper@users.noreply.github.com>
2024-12-02 10:53:25 +0000

    Update connector.py

--- adjust_report_etl/Premium/connector.py
+++ adjust_report_etl/Premium/connector.py
@@ -130,7 +130,13 @@ def update(configuration: dict, state: dict):
 
         log.info(f"Received response with status code: {response.status_code}")
         response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        log.error(f"HTTP error occurred: {err}")
+        log.error(f"Response content: {response.text}")
+    except Exception as err:
+        log.error(f"Other error occurred: {err}")
 
+    try:
         log.info("Processing Adjust data...")
         csv_reader = csv.DictReader(StringIO(response.text))
         report_data = [row for row in csv_reader]
@@ -169,15 +175,12 @@ def update(configuration: dict, state: dict):
                 },
             )
         log.info(f"Completed upserting {len(report_data)} rows to BigQuery.")
-
-    except requests.exceptions.HTTPError as err:
-        log.warning(f"HTTP error occurred: {err}")
-        log.warning(f"Response content: {response.text}")
+        
     except json.JSONDecodeError as json_err:
-        log.warning(f"JSON decoding error: {json_err}")
-        log.warning(f"Raw response content: {response.text}")
+        log.error(f"JSON decoding error: {json_err}")
+        log.error(f"Raw response content: {response.text}")
     except Exception as err:
-        log.warning(f"Other error occurred: {err}")
+        log.error(f"Other error occurred: {err}")
 
 
 connector = Connector(update=update, schema=schema)
