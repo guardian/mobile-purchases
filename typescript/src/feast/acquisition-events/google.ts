a2c195ec77ad9e6f5b1d942c8efeba440f0aaa4f
Pascal Honoré <pascal@alseyn.net>
2024-12-16 17:04:07 +0000

    refactoring

--- typescript/src/feast/acquisition-events/google.ts
+++ typescript/src/feast/acquisition-events/google.ts
@@ -4,54 +4,9 @@ import { GoogleSubscription, fetchGoogleSubscriptionV2 } from "../../services/go
 import { googlePackageNameToPlatform } from "../../services/appToPlatform";
 import { dateToSecondTimestamp, thirtyMonths } from "../../utils/dates";
 import { restClient } from "../../utils/restClient";
-import { getConfigValue } from "../../utils/ssmConfig"
-import { Stage } from "../../utils/appIdentity"
-
-// This function is duplicated from the copy in src/update-subs/google.ts
-// This will be corrected in the future refactoring
-
-type AcquisitionApiPayloadQueryParameter = {
-    name: string,
-    value: string
-}
-
-// This schema simply follows the one given here: 
-// direct link: https://github.com/guardian/support-frontend/blob/main/support-modules/acquisition-events/src/main/scala/com/gu/support/acquisitions/models/AcquisitionDataRow.scala
-// permalink  : https://github.com/guardian/support-frontend/blob/4d8c76a16bddd01ab91e59f89adbcf0867923c69/support-modules/acquisition-events/src/main/scala/com/gu/support/acquisitions/models/AcquisitionDataRow.scala
-
-type AcquisitionApiPayload = {
-    eventTimeStamp: string,
-    product: string,
-    amount?: number,
-    country: string,
-    currency: string,
-    componentId?: string,
-    componentType?: string,
-    campaignCode?: string,
-    source?: string,
-    referrerUrl?: string,
-    abTests: void[], // this will have to be updated later if we want to use it
-    paymentFrequency: string,
-    paymentProvider?: void, // this will have to be updated later if we want to use it
-    printOptions?: void, // this will have to be updated later if we want to use it
-    browserId?: string,
-    identityId?: string,
-    pageViewId?: string,
-    referrerPageViewId?: string,
-    labels: void[],
-    promoCode?: string,
-    reusedExistingPaymentMethod: boolean,
-    readerType: string,
-    acquisitionType: string,
-    zuoraSubscriptionNumber?: string,
-    contributionId?: string,
-    paymentId: string, // optional in the acquisition API model, but required by Data Design, see comment id: e3f790af 
-    queryParameters: AcquisitionApiPayloadQueryParameter[],
-    platform?: string,
-    postalCode?: string,
-    state?: string,
-    email?: string
-}
+import { getConfigValue } from "../../utils/ssmConfig";
+import { Stage } from "../../utils/appIdentity";
+import { AcquisitionApiPayload, AcquisitionApiPayloadQueryParameter } from "./types";
 
 const googleSubscriptionToSubscription = (
     purchaseToken: string,
