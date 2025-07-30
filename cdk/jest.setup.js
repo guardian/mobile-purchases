7103ba232ea615031b10bca07dc60ff2039c0bfc
andresilva-guardian <andre.silva.mindera@guardian.co.uk>
2025-07-30 16:24:28 +0100

    Revert "Add environment variable setup for Guardian CDK in jest configuration"
    This reverts commit 045d2da6b15b61672798f4232a829e36c22767f9.

--- cdk/jest.setup.js
+++ cdk/jest.setup.js
@@ -1,11 +1,1 @@
-// Set environment variables that Guardian CDK might expect
-process.env.AWS_REGION = 'eu-west-1';
-process.env.Stage = 'TEST';
-process.env.Stack = 'mobile';
-process.env.App = 'mobile-purchases';
-
-jest.mock("@guardian/cdk/lib/constants/tracking-tag", () => ({
-	TrackingTag: {
-		Value: "TEST"
-	}
-}));
+jest.mock("@guardian/cdk/lib/constants/tracking-tag");
