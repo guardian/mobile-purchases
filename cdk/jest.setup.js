045d2da6b15b61672798f4232a829e36c22767f9
andresilva-guardian <andre.silva.mindera@guardian.co.uk>
2025-07-30 16:17:57 +0100

    Add environment variable setup for Guardian CDK in jest configuration

--- cdk/jest.setup.js
+++ cdk/jest.setup.js
@@ -1,1 +1,11 @@
-jest.mock("@guardian/cdk/lib/constants/tracking-tag");
+// Set environment variables that Guardian CDK might expect
+process.env.AWS_REGION = 'eu-west-1';
+process.env.Stage = 'TEST';
+process.env.Stack = 'mobile';
+process.env.App = 'mobile-purchases';
+
+jest.mock("@guardian/cdk/lib/constants/tracking-tag", () => ({
+	TrackingTag: {
+		Value: "TEST"
+	}
+}));
