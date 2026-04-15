47285677929470508202802f381315c9fc623567
Pascal Honoré <pascal@alseyn.net>
2026-04-15 06:36:10 +0100

    stop reporting unused vars if start with underscore

--- eslint.config.js
+++ eslint.config.js
@@ -16,7 +16,11 @@ module.exports = [
       '@typescript-eslint': typescriptPlugin,
     },
     rules: {
-      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      }],
       '@typescript-eslint/no-explicit-any': 'warn',
       'no-console': 'warn',
     },
