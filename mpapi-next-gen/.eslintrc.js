efeac2a17d1de99eb69482d2cf60bc2687fc23d3
Pascal Honoré <pascal@alseyn.net>
2026-06-18 09:45:33 +0100

    add linting and formatting

---
+++ mpapi-next-gen/.eslintrc.js
@@ -0,0 +1,18 @@
+// .eslintrc.js
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    // Add any custom rules here
+    // You can enable stricter rules over time
+  },
+};
