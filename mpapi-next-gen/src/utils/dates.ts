227cbcd283bf25099d5ed7122abeb3529ea4bbe3
Pascal Honoré <pascal@alseyn.net>
2026-06-22 12:01:50 +0100

    refactoring

--- mpapi-next-gen/src/utils/dates.ts
+++ mpapi-next-gen/src/utils/dates.ts
@@ -1,5 +1,31 @@
 import type { Option } from './option';
 
+export function dateToUnixTimestamp(date: Date): number {
+	return Math.ceil(date.getTime() / 1000);
+}
+
+export function plusHours(from: Date, count: number): Date {
+	const newDate = new Date(from.getTime());
+	newDate.setUTCHours(from.getUTCHours() + count);
+	return newDate;
+}
+
+export function plusDays(from: Date, count: number): Date {
+	const newDate = new Date(from.getTime());
+	newDate.setUTCDate(from.getUTCDate() + count);
+	return newDate;
+}
+
+export function plusMonths(from: Date, count: number): Date {
+	const newDate = new Date(from.getTime());
+	newDate.setUTCMonth(from.getUTCMonth() + count);
+	return newDate;
+}
+
+export function plusThirtyMonths(from: Date): Date {
+	return plusMonths(from, 30);
+}
+
 export function optionalMsToDate(ms: Option<string> | undefined): Option<Date> {
 	if (ms) {
 		const parsedDate = new Date(Number.parseInt(ms));
@@ -11,30 +37,4 @@ export function optionalMsToDate(ms: Option<string> | undefined): Option<Date> {
 	} else {
 		return null;
 	}
-}
-
-export function thirtyMonths(from: Date = new Date()): Date {
-	return plusMonths(from, 30);
-}
-
-export function dateToSecondTimestamp(date: Date): number {
-	return Math.ceil(date.getTime() / 1000);
-}
-
-export function plusMonths(from: Date, count: number): Date {
-	const newDate = new Date(from.getTime());
-	newDate.setUTCMonth(from.getUTCMonth() + count);
-	return newDate;
-}
-
-export function plusDays(from: Date, count: number): Date {
-	const newDate = new Date(from.getTime());
-	newDate.setUTCDate(from.getUTCDate() + count);
-	return newDate;
-}
-
-export function plusHours(from: Date, count: number): Date {
-	const newDate = new Date(from.getTime());
-	newDate.setUTCHours(from.getUTCHours() + count);
-	return newDate;
 }
