777ad776ae4fbbac97687841b8e30943f37bedea
Pascal Honoré <pascal@alseyn.net>
2026-06-23 19:22:56 +0100

    correction

--- mpapi-next-gen/esbuild.config.js
+++ mpapi-next-gen/esbuild.config.js
@@ -2,6 +2,8 @@ const esbuild = require('esbuild');
 
 const entryPoints = {
 	'export-historical-data': './src/handlers/exportHistoricalData.ts',
+	'export-subscription-events-table':
+		'./src/handlers/exportSubscriptionEventsTable.ts',
 	'export-subscription-table-v2': './src/handlers/exportSubscriptionTableV2.ts',
 	googleoauth2: './src/handlers/googleOauth2.ts',
 };
