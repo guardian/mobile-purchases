6fde960b4531b155636828d8c238c912985aba0d
Pascal Honoré <pascal@alseyn.net>
2026-08-27 17:07:19 +0100

    add logging in mobile-purchases-soft-opt-in-acquisitions-DLQ-PROD (#2295)
    * add logging in mobile-purchases-soft-opt-in-acquisitions-DLQ-PROD
    
    * better error message

--- typescript/src/soft-opt-ins/dlq-processor.ts
+++ typescript/src/soft-opt-ins/dlq-processor.ts
@@ -100,10 +100,20 @@ export async function handler(_event: unknown): Promise<void> {
 				continue;
 			}
 
-			const success = await processAcquisition(subscriptionRecord, identityId);
-
-			if (success) {
-				await deleteMessage(dlqUrl, message.ReceiptHandle);
+			try {
+				console.log(
+					`[fd0c3e01] processing acquisition, subscriptionId: ${subscriptionId}, identityId: ${identityId}`,
+				);
+				const success = await processAcquisition(
+					subscriptionRecord,
+					identityId,
+				);
+				if (success) {
+					await deleteMessage(dlqUrl, message.ReceiptHandle);
+				}
+			} catch (error) {
+				console.log(`[ERROR] [952e2a60] error: ${error}`);
+				throw error;
 			}
 		}
 	}
